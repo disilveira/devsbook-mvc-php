9a6ba24b308c8937a8a0a1609e2995e8198f1039
Diego Almeida <diego.almeida@outlook.com>
2022-11-08 15:30:25 -0300

    Changed some structures and Updated login logics

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -3,16 +3,47 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class LoginController extends Controller
 {
     public function signin()
     {
-        echo 'login';
+        $flash = '';
+
+        if (!empty($_SESSION['devsbook']['flash'])) {
+            $flash = $_SESSION['devsbook']['flash'];
+            $_SESSION['devsbook']['flash'] = '';
+        }
+
+        $this->render('login', [
+            'flash' => $flash
+        ]);
+    }
+
+    public function signinPost()
+    {
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password');
+
+        if ($email && $password) {
+
+            $token = LoginHandler::verifyLogin($email, $password);
+
+            if ($token) {
+                $_SESSION['devsbook']['token'] = $token;
+                $this->redirect('/');
+            } else {
+                $_SESSION['devsbook']['flash'] = "E-mail e/ou senha não conferem.";
+                $this->redirect('/login');
+            }
+        } else {
+            $this->redirect('/login');
+        }
     }
 
     public function signup()
     {
-        echo 'cadastro';
+        echo 'Tela de cadastro';
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,8 @@ use core\Router;
 
 $router = new Router();
 
+// Signin and Signup Routes
 $router->get('/', 'HomeController@index');
 $router->get('/login', 'LoginController@signin');
+$router->post('/login', 'LoginController@signinPost');
 $router->get('/cadastro', 'LoginController@signup');
