7307b1e8956dc0de73d8a2ada4496267e3aa614f
Diego Almeida <diego.almeida@outlook.com>
2022-11-08 11:19:05 -0300

    Configured the database connection

--- src/Config.php
+++ src/Config.php
@@ -1,15 +1,14 @@
 <?php
 namespace src;
 
-class Config {
-    const BASE_DIR = '/mvc/public';
-
+class Config
+{
+    const BASE_DIR = '/devsbook/public';
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test';
-    CONST DB_USER = 'root';
+    const DB_DATABASE = 'devsbook';
+    const DB_USER = 'root';
     const DB_PASS = '';
-
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
 }
